736ed9dc191b9819125210228a33c6d094317964
Eduardo Mauricio Mueller Nicastro <edu.nicastro@gmail.com>
2018-03-25 22:05:17 -0300

    Commit casa do ermano

--- views/formulario.php
+++ views/formulario.php
@@ -1,6 +1,18 @@
 <form id="insertAddressForm" class="addressForm">
-  <input type="text" name="nome">
-  <input type="text" name="sobrenome">
-  <input type="text" name="endereco">
-  <button type="submit">Salvar</button>
+  <fieldset id="personal-details">
+    <legend>Personal Details</legend>
+    <div class="input-container" id="first-name">
+
+    </div>
+    <label for="nome">Nome:
+      <input type="text" name="nome"><br />
+    </label>
+    <label for="sobrenome">Sobrenome:
+      <input type="text" name="sobrenome"><br />
+    </label>
+    <label for="endereco">Endereço:
+      <input type="text" name="endereco"><br />
+    </label>
+    <button type="submit">Salvar</button>
+  </fieldset>
 </form>
